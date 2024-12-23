2eb1e17fe422421f68180320503ac7c5b16931d1
Aditya Raj <adityaraj92.20@gmail.com>
2024-12-23 09:07:00 +0530

    feat(feature): added tech stack web page

---
+++ src/app/tech-stack/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import TechStackCard from '@/components/TechStackCard';
+
+const TechStack: React.FC = () => (
+    <div className="size-full">
+        <TechStackCard />
+    </div>
+);
+
+export default TechStack;
