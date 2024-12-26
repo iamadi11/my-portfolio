44be63238ad29066d9005b01aeb38513862cdf53
Aditya Raj <adityaraj92.20@gmail.com>
2024-12-26 11:10:25 +0530

    feat(feature): change page color on contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -8,7 +8,7 @@ import { FaLinkedin, FaMailBulk, FaPhoneAlt } from 'react-icons/fa';
 import { FaGithub } from 'react-icons/fa6';
 
 const Contact: React.FC = () => (
-    <div className="flex size-full flex-col items-center justify-center gap-8 bg-black py-8">
+    <div className="flex size-full flex-col items-center justify-center gap-8 py-8">
         <div className="text-center text-5xl">
             <div className="inline-block bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-4xl font-extrabold text-transparent">
                 If you have any questions, feel free to reach out:
